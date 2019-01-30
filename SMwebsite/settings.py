2d44d398ccfcb0f8f022155d8d1e79e6f351b480
PratibhaShrivastav <pratibha.shrivastav99@gmail.com>
2019-01-30 22:43:16 +0530

    accounts section : profile details page added

--- SMwebsite/settings.py
+++ SMwebsite/settings.py
@@ -129,6 +129,7 @@ STATIC_ROOT='/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static'),
     ]
-
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 LOGIN_REDIRECT_URL='home'
 LOGOUT_REDIRECT_URL='home'
--- accounts/models.py
+++ accounts/models.py
@@ -17,7 +17,7 @@ class Profile(models.Model):
     user = models.ForeignKey(auth.models.User,on_delete=models.CASCADE)
     gender = models.CharField(max_length=2, choices=GENDER_OPTIONS)
     contact = models.CharField(max_length=10)
-    profile_pic = models.ImageField(upload_to='images/profile_pictures',default='images/default.png')
+    profile_pic = models.ImageField(upload_to='images/',default='images/default.png',null=True,blank=True)
 
     def __str__(self):
         return self.user.username
